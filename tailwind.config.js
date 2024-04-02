924ad141bba0a139b95221cadda0f2974f80121f
Vinicius Souza <viniciuspzt@gmail.com>
2024-04-03 00:34:44 +0100

    create and fill the table with faker-js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
